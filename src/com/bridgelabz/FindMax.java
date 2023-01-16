4d4df51032dfd7614810950a1e079216cf32443d
jitain1 <singhjitainj@gmail.com>
2023-01-16 17:33:54 +0530

    Added: Find Max Integer Using CompareToo Method

--- src/com/bridgelabz/FindMax.java
+++ src/com/bridgelabz/FindMax.java
@@ -2,9 +2,22 @@ package com.bridgelabz;
 
 public class FindMax {
 
+	int checkMaximum(Integer x, Integer y, Integer z) {
+		int max = x; //Assuming x is the largest (initially)
+
+		if (y.compareTo(max) > 0)
+			max = y; // y is the largest so far
+
+		if (z.compareTo(max) > 0)
+			max = z; // now z is the largest
+
+		return  max;
+	}
+
 	public static void main(String[] args) {
+		FindMax obj = new FindMax();
+		System.out.println("The Maximum Integer Is: " + obj.checkMaximum(2, 4, 7));
 		
-		System.out.println("Welcome to Generics Problems");
 	}
 
 }
