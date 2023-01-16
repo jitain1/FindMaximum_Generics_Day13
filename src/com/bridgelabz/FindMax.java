d4eb392b1bbc092f820e7d17d6b49c4347ad7fbb
jitain1 <singhjitainj@gmail.com>
2023-01-16 17:55:08 +0530

    Added: Refactor1 Generic Method added.

--- src/com/bridgelabz/FindMax.java
+++ src/com/bridgelabz/FindMax.java
@@ -2,8 +2,8 @@ package com.bridgelabz;
 
 public class FindMax {
 
-	String checkMaximum(String x, String y, String z) {
-		String max = x; //Assuming x is the largest (initially)
+	<E extends Comparable<E>> E checkMaximum(E x, E y, E z) {
+		E max = x; //Assuming x is the largest (initially)
 
 		if (y.compareTo(max) > 0)
 			max = y; // y is the largest so far
@@ -15,9 +15,12 @@ public class FindMax {
 	}
 
 	public static void main(String[] args) {
-		FindMax obj = new FindMax();
-		System.out.println("The maximum String is : " + obj.checkMaximum("Apple", "Peach", "Banana"));
+		FindMax Obj = new FindMax();
+		System.out.println("The maximum Integer is : " +Obj.checkMaximum(1, 3, 5));
+		
+		System.out.println("The maximum Float is : " + Obj.checkMaximum(2.25, 5.480, 7.55));
+		
+		System.out.println("The maximum String is : " + Obj.checkMaximum("Apple", "Peach", "Banana"));
 		
 	}
-
 }
